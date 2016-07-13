2e830715b96e6e662edceadbf0e72f86e43f194b
brainiaconly <maxonly9@gmail.com>
2016-07-14 00:37:16 +0300

    add IPictureManager

---
+++ WcfImageViewer.Contracts/ServiceContracts/IPictureManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using WcfImageViewer.Contracts.DataContracts;
+
+namespace WcfImageViewer.Contracts.ServiceContracts
+{
+    [ServiceContract]
+    public interface IPictureManager
+    {
+        [OperationContract]
+        PictureInfo[] GetAll();
+
+        [OperationContract]
+        [FaultContract(typeof(FileNotFoundException))]
+        Stream Get(string name);
+
+        [OperationContract]
+        [FaultContract(typeof(ArgumentException))]
+        void Upload(FileUploadMessage picture);
+    }
+}
