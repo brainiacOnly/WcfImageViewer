20ce525c79f058c876ea815630ed5428fc6bcdd4
brainiaconly <maxonly9@gmail.com>
2016-07-15 01:00:53 +0300

    read image from proxy in the client

--- WcfImageVeiwer.Client/Controllers/HomeController.cs
+++ WcfImageVeiwer.Client/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WcfImageVeiwer.Client.Models;
 using WcfImageVeiwer.Client.Proxies;
+using WcfImageViewer.Contracts.DataContracts;
 
 namespace WcfImageVeiwer.Client.Controllers
 {
@@ -12,8 +15,35 @@ namespace WcfImageVeiwer.Client.Controllers
         public ActionResult Index()
         {
             var proxy = new PictureManagerClient();
-
+            var pictures = proxy.GetAll();
+            var model = new PageModel {Pictures = new List<PictureInfo>()};
+            if (pictures.Any())
+            {
+                model.Pictures = pictures;
+                var imageStream = proxy.Get(pictures.First().Name);
+                model.ImageBase64String = Convert.ToBase64String(ReadFileStream(imageStream));
+            }
             return View();
+        }
+
+        private byte[] ReadFileStream(Stream source)
+        {
+            int bufferLen = 100000;
+            byte[] buffer = new byte[bufferLen];
+            MemoryStream targetStream = null;
+
+            using (targetStream = new MemoryStream())
+            {
+                int count = 0;
+                while ((count = source.Read(buffer, 0, bufferLen)) > 0)
+                {
+                    targetStream.Write(buffer, 0, count);
+                }
+                targetStream.Close();
+                source.Close();
+            }
+
+            return targetStream.ToArray();
         }
     }
 }
---
+++ WcfImageVeiwer.Client/Models/PageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WcfImageViewer.Contracts.DataContracts;
+
+namespace WcfImageVeiwer.Client.Models
+{
+    public class PageModel
+    {
+        public IEnumerable<PictureInfo> Pictures { get; set; }
+
+        public string ImageBase64String { get; set; }
+    }
+}
