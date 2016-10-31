43c32f3ca63fbe32508f3c32543dfb10ec40619b
brainiaconly <maxonly9@gmail.com>
2016-10-31 13:13:28 +0200

    add files to the source control

---
+++ WcfImageVeiwer.Client/Proxies/IPictureManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WcfImageViewer.Contracts.DataContracts;
+
+namespace WcfImageVeiwer.Client.Proxies
+{
+    [ServiceContract]
+    public interface IPictureManager
+    {
+        [OperationContract]
+        PictureInfo[] GetAll();
+
+        [OperationContract]
+        Stream Get(string name);
+
+        [OperationContract]
+        void Upload(FileUploadMessage picture); 
+
+        [OperationContract]
+        Task<PictureInfo[]> GetAllAsync();
+
+        [OperationContract]
+        Task<Stream> GetAsync(string name);
+
+        [OperationContract]
+        Task UploadAsync(FileUploadMessage picture);
+    }
+}
---
+++ WcfImageVeiwer.Client/Proxies/PictureManager.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using WcfImageViewer.Contracts;
+using WcfImageViewer.Contracts.DataContracts;
+using WcfImageViewer.Contracts.ServiceContracts;
+
+namespace WcfImageVeiwer.Client.Proxies
+{
+    public class PictureManager : ClientBase<IPictureManager>, IPictureManager
+    {
+        public PictureInfo[] GetAll()
+        {
+            return Channel.GetAll();
+        }
+
+        public Stream Get(string name)
+        {
+            return Channel.Get(name);
+        }
+
+        public void Upload(FileUploadMessage picture) 
+        {
+            Channel.Upload(picture);
+        }
+
+        public Task<PictureInfo[]> GetAllAsync()
+        {
+            return Channel.GetAllAsync();
+        }
+
+        public Task<Stream> GetAsync(string name)
+        {
+            return Channel.GetAsync(name);
+        }
+
+        public Task UploadAsync(FileUploadMessage picture)
+        {
+            return Channel.UploadAsync(picture);
+        }
+    }
+}
