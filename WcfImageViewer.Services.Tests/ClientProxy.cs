19ea772a262c3eb97fd01f1f2c9116dd3a8cc4ad
brainiaconly <maxonly9@gmail.com>
2016-10-31 13:25:30 +0200

    add ClientProxy.cs to the source control

---
+++ WcfImageViewer.Services.Tests/ClientProxy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using WcfImageViewer.Contracts.DataContracts;
+using WcfImageViewer.Contracts.ServiceContracts;
+
+namespace WcfImageViewer.Services.Tests
+{
+    class ClientProxy : ClientBase<IPictureManager>, IPictureManager
+    {
+        public PictureInfo[] GetAll()
+        {
+            return Channel.GetAll();
+        }
+
+        public Stream Get(string name)
+        {
+            return Channel.Get(name);
+        }
+
+        public void Upload(FileUploadMessage picture)
+        {
+            Channel.Upload(picture);
+        }
+    }
+}
